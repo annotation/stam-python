0bb475b8c0fbf4200eb293575550fcd341b8d093
Maarten van Gompel <proycon@anaproy.nl>
2023-04-15 20:44:52 +0200

    fixup

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use crate::annotationstore::PyAnnotationStore;
 use crate::error::PyStamError;
 use crate::resources::{PyCursor, PyOffset, PyTextResource};
 use crate::selector::{PySelector, PySelectorKind};
-use crate::textselection::PyTextSelection;
+use crate::textselection::{PyTextSelection, PyTextSelectionOperator};
 
 #[pymodule]
 pub fn stam(py: Python<'_>, m: &PyModule) -> PyResult<()> {
@@ -34,5 +34,6 @@ pub fn stam(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyOffset>()?;
     m.add_class::<PyCursor>()?;
     m.add_class::<PyTextSelection>()?;
+    m.add_class::<PyTextSelectionOperator>()?;
     Ok(())
 }
